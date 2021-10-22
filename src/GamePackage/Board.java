c793490ad614a999f370232f712ef6e3a8e13ebc
EmilWikell <emil.s.wikell@gmail.com>
2021-10-22 12:10:48 +0200

    creating Panels and a button

--- src/GamePackage/Board.java
+++ src/GamePackage/Board.java
@@ -1,7 +1,14 @@
 package GamePackage;
 
-public class Board {
+import javax.swing.*;
+
+public class Board extends JFrame {
     public static void main(String[] args) {new Board();}
+
+    JPanel rootPanel = new JPanel();
+    JPanel gamePanel = new JPanel();
+    JPanel settingsPanel = new JPanel();
+    JButton newGame = new JButton("New Game");
 
     public Board(){
 
