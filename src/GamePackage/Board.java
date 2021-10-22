9660f105f63da246b6d80fb4862076dab7db9fee
Philip Tonaczew <58476139+Tyareenbajka@users.noreply.github.com>
2021-10-22 12:45:58 +0200

    added move logic rules

--- src/GamePackage/Board.java
+++ src/GamePackage/Board.java
@@ -2,6 +2,7 @@ package GamePackage;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
 
 public class Board extends JFrame {
     public static void main(String[] args) {new Board();}
@@ -25,4 +26,27 @@ public class Board extends JFrame {
         //setResizable(false);
         //setDefaultCloseOperation(EXIT_ON_CLOSE);
     }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        Button t = (Button) e.getSource();
+        if(e.getSource() == t && check(t)){
+            String buttonText = t.getText();
+            t.setText("");
+            t.setBackground(Color.black);
+            t.setEnabled(false);
+            black.setText(buttonText);
+            black.setBackground(this.getBackground());
+            black.setEnabled(true);
+            black = t;
+        }
+    }
+
+    private boolean check(Button button) {
+        int x = button.getxCoordinate() - black.getxCoordinate();
+        int y = button.getyCoordinate() - black.getyCoordinate();
+        return ((Math.abs(x) == 1 && y == 0) || (x == 0 && Math.abs(y) == 1));
+    }
+
+
 }
