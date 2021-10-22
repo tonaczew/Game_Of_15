2ef9be4c77f2f3fe923b1bc58ee4d9755ba267b1
EmilWikell <emil.s.wikell@gmail.com>
2021-10-22 12:16:03 +0200

    placing panels and button. Frame visibility

--- src/GamePackage/Board.java
+++ src/GamePackage/Board.java
@@ -1,6 +1,7 @@
 package GamePackage;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class Board extends JFrame {
     public static void main(String[] args) {new Board();}
@@ -11,6 +12,17 @@ public class Board extends JFrame {
     JButton newGame = new JButton("New Game");
 
     public Board(){
+        rootPanel.setLayout(new BorderLayout());
+        gamePanel.setLayout(new GridLayout(4, 4));
+        add(rootPanel);
 
+        rootPanel.add(settingsPanel, BorderLayout.EAST);
+        rootPanel.add(gamePanel);
+        settingsPanel.add(newGame);
+
+        setSize(600,500);
+        setVisible(true);
+        setResizable(false);
+        //setDefaultCloseOperation(EXIT_ON_CLOSE);
     }
 }
