d1b5895c8fd08b8db61cb0df1fc989a5c1cbef83
Philip Tonaczew <58476139+Tyareenbajka@users.noreply.github.com>
2021-10-22 12:11:17 +0200

    added final to x,y position

--- src/GamePackage/Button.java
+++ src/GamePackage/Button.java
@@ -4,8 +4,8 @@ import javax.swing.*;
 
 public class Button extends JButton {
 
-    private int xPosition;
-    private int yPosition;
+    private final int xPosition;
+    private final int yPosition;
 
     public Button(String text, int xPosition, int yPosition) {
         super(text);
