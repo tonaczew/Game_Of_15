9af24531b79e33c5731ebef964d6f3d6c663e9d8
Philip Tonaczew <58476139+Tyareenbajka@users.noreply.github.com>
2021-10-28 12:34:35 +0200

    class Game done + moved main method from class Board to class Game

--- src/GamePackage/Game.java
+++ src/GamePackage/Game.java
@@ -8,45 +8,43 @@ import java.awt.event.ActionListener;
 public class Game extends JFrame implements ActionListener {
 
     JPanel menu;
-    JPanel rootPanel;
-    JButton start = new JButton("start new game");
-    JButton demo = new JButton("Demo win");
+    JButton start;
+    JButton demo;
 
     public Game(){
-
-        rootPanel = new JPanel();
-        menu = new JPanel();
-
-        menu.add(start);
-        menu.add(demo);
-
+        initializeGame();
         demo.addActionListener(this);
         start.addActionListener(this);
-
-
-        rootPanel.add(menu, BorderLayout.EAST);
-
-        this.setLayout(new GridLayout(1,2));
-        this.setVisible(true);
-        this.setResizable(false);
-        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
-        this.add(rootPanel);
-        this.pack();
-    }
-
-
-    public static void main(String[] args) {
-        new Game();
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
         if(e.getSource() == start){
             this.setVisible(false);
-            new Board();
+            new Board(false);
         } else if(e.getSource() == demo){
-            new Board();
+            new Board(true);
         }
+    }
 
+    private void initializeGame() {
+        start  = new JButton("start new game");
+        demo = new JButton("Demo win");
+        menu = new JPanel();
+
+        menu.setLayout(new FlowLayout());
+        menu.add(start);
+        menu.add(demo);
+
+        this.setSize(new Dimension(300,300));
+        this.setLayout(new GridLayout(1,2));
+        this.setVisible(true);
+        this.setResizable(false);
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        this.add(menu);
+    }
+
+    public static void main(String[] args) {
+        new Game();
     }
 }
